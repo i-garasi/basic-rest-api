a61a4e7d7e5cf3a749ae823b0a62c03e97c54d1e
igarashi kazuki <sousiki.links@gmail.com>
2021-09-20 14:17:56 +0900

    ✨ ユーザー検索機能の追加

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,4 +1,8 @@
 const indexModule = (() => {
+  // 検索ボタンクリック時のイベントリスナーと登録
+  document.getElementById("searchBtn").addEventListener("click", () => {
+    return searchModule.searchUsers();
+  });
   // usersモジュールのfetchAllUsersメソッドを呼び出す
   return usersModule.fetchAllUsers();
 })();
