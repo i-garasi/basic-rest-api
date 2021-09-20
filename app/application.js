b37a5c546b9bace3eeefa66b63101d7e77ab64a5
igarashi kazuki <sousiki.links@gmail.com>
2021-09-20 13:05:24 +0900

    ✨ ユーザーの取得（全件、単一、検索）

--- app/application.js
+++ app/application.js
@@ -1,10 +1,42 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const sqlite3 = require("sqlite3");
+const dbPath = "app/db/database.sqlite3";
 
-app.get('/api/v1/hello', (req, res) => {
-  res.json({"message": "hello, world!"})
-})
+// get all user
+app.get("/api/v1/users", (req, res) => {
+  // connect database
+  const db = new sqlite3.Database(dbPath);
+  db.all("SELECT * FROM users", (err, rows) => {
+    res.json(rows);
+  });
+  db.close();
+});
+
+// get single user
+app.get("/api/v1/users/:id", (req, res) => {
+  // connect database
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    res.json(row);
+  });
+  db.close();
+});
+
+// get search user
+app.get("/api/v1/search", (req, res) => {
+  // connect database
+  const db = new sqlite3.Database(dbPath);
+  const val = req.query.q;
+
+  db.all(`SELECT * FROM users WHERE name LIKE "%${val}%"`, (err, rows) => {
+    res.json(rows);
+  });
+  db.close();
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port)
-console.log("Listen on port: " + port)
+app.listen(port);
+console.log("Listen on port: " + port);
